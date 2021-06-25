3a128019e75d2be4ac981a2a3970142acfe64a5e
Pradnyoday Mirajkar <pradnyoday@gmail.com>
2021-06-26 05:19:00 +0530

    Added Github-actions

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    context = models.CharField(max_length=2050)
+    slug = models.SlugField(max_length=250)
+
+    def __str__(self) -> str:
+        return self.title
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Post
+
 
 # Create your tests here.
+class ModelTesting(TestCase):
+    def setUp(self) -> None:
+        self.blog = Post.objects.create(title='First Post', author='Paddy', context='First Post!!', slug='First Post')
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'First Post')
